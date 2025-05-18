3d44b9a9a26ae4975527158969df24712d63b093
Naman Tiwari <145788363+Sciber766@users.noreply.github.com>
2025-05-18 04:25:40 +0000

    tweaks and js

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
 function redirectTo(page) {
     window.location.href = page;
 }
+
+let data = {}
+
+function loadData (){
+
+}
+function displayBloodGroup(data){
+    let bloodGroup = document.querySelector("#Bgroup");
+    console.log(bloodGroup.value);
+    bloodGroup.value = data;
+    bloodGroup.innerText = data;
+}
+
+function displayStatus(data){
+    let Status = document.querySelector("#DonationStatus");
+    console.log(Status.value);
+    if (data == "1") {
+        Status.value = 'Available';
+        Status.innerText = 'Available';
+    }
+    else if (data == "0") {
+        Status.value = 'Not Available';
+        Status.innerText = 'Not Available';
+    }
+    else {
+        Status.value = 'Unknown';
+        Status.innerText = 'Unknown';
+    }
+    
+}
+
+function displayDOB(data) {
